0a2558cccf8a85c83bb4a7ea4b277bd50f791a3e
Karla Ramirez <karlar1824@gmail.com>
2020-12-09 14:29:36 -0500

    new player script and bullet

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    public float speed = 30;
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.up * speed;
         
     }
 
+    void OntriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+   void OnBecomeInvisible()
+   {
+       Destroy(gameObject);
+   }
+   
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
+   
 {
+   
+    public float speed = 30;
+
+    public GameObject theBullet;
+
+
+    void FixedUpdate(){
+        float horzMove = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horzMove,0) * speed * Time.deltaTime;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+     
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Jump"))
+        {
+            Instantiate(theBullet,transform.position, Quaternion.identity);    //Quaternion make sure the bullets don't rotate
+        }
     }
 }
